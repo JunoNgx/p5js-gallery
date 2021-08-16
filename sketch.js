a2dab8ea43c46378851461feac02a44cb1d5f261
Juno Nguyen <juno.ngx@gmail.com>
2021-08-16 15:46:08 +0800

    Refactor

--- sketch.js
+++ sketch.js
@@ -7,10 +7,10 @@ let shapes;
 const G = {
     SPEED_MIN: 0.5,
     SPEED_MAX: 1.5,
-    BASE_MIN: 0.1,
-    BASE_MAX: 0.2,
-    HEIGHT_MIN: 0.2,
-    HEIGHT_MAX: 0.6
+    BASE_MIN: 0.2,
+    BASE_MAX: 0.5,
+    HEIGHT_MIN: 0.4,
+    HEIGHT_MAX: 0.8
 }
 
 function setup() {
@@ -19,7 +19,7 @@ function setup() {
     C.mid = createVector(C.width*0.5, C.height*0.5);
 
     shapes = [];
-    for (let i = 0; i < 15; i++) {
+    for (let i = 0; i < 5; i++) {
         shapes.push({
             pos: createVector(random(0, C.width), random(0, C.height)),
             base: random(C.width * G.BASE_MIN, C.width * G.BASE_MAX),
@@ -52,23 +52,33 @@ function draw() {
         const tr = createVector(s.pos.x + s.base, s.pos.y);
         const bl = createVector(s.pos.x - s.height*tan(s.angle-HALF_PI), s.pos.y + s.height);
         const br = createVector(bl.x + s.base, s.pos.y + s.height);
-        color(255, 0, 0);
         noStroke();
         quad(tl.x, tl.y, tr.x, tr.y, br.x, br.y, bl.x, bl.y);
+        
+        // color(255, 0, 0);
+        // circle(s.pos.x, s.pos.y, 10);
 
-        if (s.pos.y > C.height) {
-            s.pos.x = random(C.width * 0.5, C.width*1.3);
-            s.pos.y = random(C.height * -0.5, C.height * -1.5);
-            s.base = random(C.width * G.BASE_MIN, C.width * G.BASE_MAX);
-            s.height = random(C.height * G.HEIGHT_MIN, C.height * G.HEIGHT_MAX);
-            s.speed = random(G.SPEED_MIN, G.SPEED_MAX);
-        }
+        // if (s.pos.y > C.height) {
+        //     s.pos.x = random(C.width * 0.4, C.width*1.3);
+        //     s.pos.y = random(C.height * -0.5, C.height * -1.5);
+        //     s.base = random(C.width * G.BASE_MIN, C.width * G.BASE_MAX);
+        //     s.height = random(C.height * G.HEIGHT_MIN, C.height * G.HEIGHT_MAX);
+        //     s.speed = random(G.SPEED_MIN, G.SPEED_MAX);
+        // }
     });
-
+    shapes = shapes.filter(s => s.pos.y < C.height);
 
     textAlign(CENTER);
     textSize(width*0.15);
     text("Juno Nguyen", C.mid.x, C.mid.y);
+}
 
-    console.log(shapes.length);
+function paral(_pos, _base, _height, _speed) {
+    return {
+        pos: _pos,
+        base: _base,
+        height: _height,
+        speed: _speed,
+        angle: -PI/1.5
+    };
 }
