cd35552629d7f2b6d11823c5eebb33fbd33124bd
Juno Nguyen <juno.ngx@gmail.com>
2021-08-20 17:47:42 +0800

    Implement heartbeat

--- docs/new/sketch.js
+++ docs/new/sketch.js
@@ -3,8 +3,8 @@ const G = {
     SQUARE_SIZE: 0.05,
     SQUARE_MOV_RADIUS: 0.2,
     HEART_RADIUS: 0.12,
-    BEAT_RADIUS_INIT: 0.35,
-    BEAT_RADIUS_FINAL: 0.42
+    BEAT_RADIUS_INIT: 0.32,
+    BEAT_RADIUS_CHANGE: 0.1
 };
 
 /** @type { {s: number, l: number} } */
@@ -69,14 +69,32 @@ function draw() {
     triangle(triLeft.x, triLeft.y, triRight.x, triRight.y, triBottom.x, triBottom.y);
     
     // Draw the beat
-    let br
-    br = size.s * G.BEAT_RADIUS_INIT
-        + size.s * (G.BEAT_RADIUS_FINAL - G.BEAT_RADIUS_INIT)
-        * easeOutExpo(progress);
+    let br;
+    const heartAnimMark = 0.7;
+    if (progress <= heartAnimMark) {
+        br = easeInElastic(
+            time,
+            size.s * G.BEAT_RADIUS_INIT,
+            size.s * G.BEAT_RADIUS_CHANGE,
+            G.ANIM_TIME * heartAnimMark
+        );
+    } else {
+        br = easeOutExpo(
+            time - (G.ANIM_TIME * heartAnimMark),
+            size.s * (G.BEAT_RADIUS_INIT + G.BEAT_RADIUS_CHANGE),
+            -size.s * G.BEAT_RADIUS_CHANGE,
+            G.ANIM_TIME * (1-heartAnimMark)
+        );
+    }
+
     noFill();
     stroke('dddddd');
     strokeWeight(2);
     arc(mid.x, mid.y, br, br, 0, PI*2);
+
+
+    fill("#eee")
+    text(progress, 500, 500);
 }
 
 /**
@@ -93,4 +111,49 @@ function easeOutExpo(x) {
 
     // easeOutCubic
     // return 1 - pow(1 - x, 3);
+}
+
+/**
+ * 
+ * @param {number} t Time
+ * @param {number} b Initial value
+ * @param {number} c Change in value
+ * @param {number} d Duration
+ * @returns 
+ */
+function easeInOutElastic (t, b, c, d) {
+    var s = 1.70158;
+    var p = 0;
+    var a = c;
+    if (t == 0) return b;
+    if ((t /= d / 2) == 2) return b + c;
+    if (!p) p = d * (.3 * 1.5);
+    if (a < Math.abs(c)) {
+        a = c;
+        var s = p / 4;
+    }
+    else var s = p / (2 * Math.PI) * Math.asin(c / a);
+    if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+    return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
+}
+
+// @ts-ignore
+function easeInElastic (t, b, c, d) {
+    var s = 1.70158;
+    var p = 0;
+    var a = c;
+    if (t == 0) return b;
+    if ((t /= d) == 1) return b + c;
+    if (!p) p = d * .3;
+    if (a < Math.abs(c)) {
+        a = c;
+        var s = p / 4;
+    }
+    else var s = p / (2 * Math.PI) * Math.asin(c / a);
+    return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+}
+
+// @ts-ignore
+function easeOutExpo (t, b, c, d) {
+    return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
 }
