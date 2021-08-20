ba516b7c33f48cc9e7d2f45bd838e7048c20ac03
Juno Nguyen <juno.ngx@gmail.com>
2021-08-20 19:49:27 +0800

    Update template

--- docs/_template/sketch.js
+++ docs/_template/sketch.js
@@ -1,31 +1,23 @@
 const G = {
 
 };
-/** @type {number} */
+/** @type { {s: number, l: number} } */
 let size;
+/** @type { import("p5").Vector } */
+let mid;
+/** @type { number } */
+let time;
 
 function setup() {
-    size = (windowWidth > windowHeight) ? windowWidth : windowHeight;
-
+    size = {
+        l: (windowWidth >= windowHeight) ? windowWidth : windowHeight,
+        s: (windowWidth <= windowHeight) ? windowWidth : windowHeight
+    };
+    mid = createVector(windowWidth * 0.5, windowHeight * 0.5);
+    time = 0;
     createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-
-}
-
-/**
- * @param {number} x 
- * @returns {number}
- */
-function easeFunc(x) {
-
-    // easeOutExpo
-    return x === 1 ? 1 : 1 - pow(2, -10 * x);
-
-    // easeOutQuad
-    // return 1 - pow(1 - x, 4);
-
-    // easeOutCubic
-    // return 1 - pow(1 - x, 3);
+    time++;
 }
