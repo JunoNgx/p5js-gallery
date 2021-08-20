c2650dc8ecac490ddd4488502bfd083c7053d423
Juno Nguyen <juno.ngx@gmail.com>
2021-08-20 17:48:10 +0800

    Delete unused function

--- docs/new/sketch.js
+++ docs/new/sketch.js
@@ -121,23 +121,6 @@ function easeOutExpo(x) {
  * @param {number} d Duration
  * @returns 
  */
-function easeInOutElastic (t, b, c, d) {
-    var s = 1.70158;
-    var p = 0;
-    var a = c;
-    if (t == 0) return b;
-    if ((t /= d / 2) == 2) return b + c;
-    if (!p) p = d * (.3 * 1.5);
-    if (a < Math.abs(c)) {
-        a = c;
-        var s = p / 4;
-    }
-    else var s = p / (2 * Math.PI) * Math.asin(c / a);
-    if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
-    return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
-}
-
-// @ts-ignore
 function easeInElastic (t, b, c, d) {
     var s = 1.70158;
     var p = 0;
