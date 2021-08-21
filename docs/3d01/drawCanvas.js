48aa45ce711b4a9cdfa92a48f41fd948e024302c
Juno Nguyen <juno.ngx@gmail.com>
2021-08-21 14:07:03 +0800

    Implement handling functions

--- docs/3d01/drawCanvas.js
+++ docs/3d01/drawCanvas.js
@@ -13,20 +13,36 @@ function loop() {
     let currentTime = (new Date()).getTime();
     let deltaTime = currentTime - lastTime;
     if (deltaTime > INTERVAL) {
-        // update(deltaTime);
         draw();
         lastTime = currentTime - (deltaTime % INTERVAL)
     }
     window.requestAnimationFrame(loop);
 }
 
-// function update(deltaTime) {
-
-// }
-
 window.onload = setup;
 
+/**
+ * @typedef {{
+ * x: number,
+ * y: number
+ * }} Vector2D
+ */
 
+/**
+ * @typedef {{
+ * x: number,
+ * y: number,
+ * z: number
+ * }} Vector3D
+ */
+
+/**
+ * @typedef {{
+ * pos: Vector3D,
+ * radius: number,
+ * rotation: Vector3D
+ * }} Tetrahedron
+ */
 
 function setup() {
 
@@ -40,15 +56,56 @@ function setup() {
 function draw() {
     // Clear screen
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-
 }
 
 /**
  * 
  * @param {number} _x 
  * @param {number} _y 
- * @returns {Vector}
+ * @param {number} _z 
+ * @param {number} _radius 
  */
-function vec(_x, _y) {
+function tetrahedron(_x, _y, _z, _radius) {
+    return {
+        pos: vec3(_x, _y, _z),
+        radius: _radius,
+        rotation: vec3(0, 0, 0)
+    }
+}
+
+/**
+ * 
+ * @param {Tetrahedron} t 
+ */
+function drawTetrahedron(t) {
+    /** @type {Vector3D} */
+    let top = vec3(
+        t.pos.x + t.radius * Math.cos(Math.PI + t.rotation.z),
+        t.pos.y + t.radius * Math.sin(Math.PI + t.rotation.z),
+        t.pos.z + 
+    );
+    let botA;
+    let botB;
+    let botC;
+}
+
+/**
+ * 
+ * @param {number} _x 
+ * @param {number} _y 
+ * @returns {Vector2D}
+ */
+function vec2(_x, _y) {
     return { x: _x, y: _y };
+}
+
+/**
+ * 
+ * @param {number} _x 
+ * @param {number} _y 
+ * @param {number} _z 
+ * @returns {Vector3D}
+ */
+ function vec3(_x, _y, _z) {
+    return { x: _x, y: _y, z: _z };
 }
