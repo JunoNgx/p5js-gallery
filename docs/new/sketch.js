32be39d8ce54d7e35debcef3bd37b2a7e84ac362
Juno Nguyen <juno.ngx@gmail.com>
2021-08-24 14:27:33 +0800

    Clean up

--- docs/new/sketch.js
+++ docs/new/sketch.js
@@ -7,8 +7,6 @@ const G = {
 let size;
 /** @type { import("p5").Vector } */
 let mid;
-/** @type { number } */
-let time;
 
 /**
  * @typedef {{
@@ -25,14 +23,11 @@ function setup() {
         s: (windowWidth <= windowHeight) ? windowWidth : windowHeight
     };
     mid = createVector(windowWidth * 0.5, windowHeight * 0.5);
-    time = 0;
 
     const column = (windowWidth - (windowWidth * G.SQUARE_GAP * 2))/
         (windowWidth * (G.SQUARE_GAP + G.SQUARE_SIZE_X));
     const row = (windowHeight - (windowHeight * G.SQUARE_GAP * 2))/
         (windowHeight * (G.SQUARE_GAP + G.SQUARE_SIZE_Y));
-
-    // console.log((windowHeight - (windowHeight * G.SQUARE_GAP * 2)));
 
     squares = [];
     for (let i = 0; i < column; i++) {
@@ -47,13 +42,6 @@ function setup() {
                     y: windowHeight * G.SQUARE_SIZE_Y,
                 }
             })
-            // squares.push({
-            //     pos: createVector(
-            //         windowWidth * G.SQUARE_GAP + i * windowWidth * (G.SQUARE_SIZE/2 + G.SQUARE_GAP),
-            //         windowHeight * G.SQUARE_GAP + j * windowHeight * (G.SQUARE_SIZE/2 + G.SQUARE_GAP)
-            //     ),
-            //     size: size.l * G.SQUARE_SIZE
-            // })
         }
     }
 
@@ -63,15 +51,6 @@ function setup() {
 function draw() {
     background('#ddd');
     
-    time++;
-
-    // for (let i = 0; i<100; i++) {
-    //     line(
-    //         i * (size.l * G.SQUARE_SIZE/2), -100,
-    //         i * (size.l * G.SQUARE_SIZE/2), 1000,
-    //     )
-    // }
-
     rectMode(CENTER);
     squares.forEach( s => {
 
@@ -80,13 +59,8 @@ function draw() {
             ? (0.1 + 0.9 * (dist / (size.l * 0.2)))
             : 1;
 
-
         fill(44);
-        // rect(s.pos.x, s.pos.y, size.l * (G.SQUARE_SIZE + G.SQUARE_GAP), size.l * (G.SQUARE_SIZE + G.SQUARE_GAP));
         rect(s.pos.x, s.pos.y, s.size.x * sizeRate, s.size.y * sizeRate);
-        // ellipse(s.pos.x, s.pos.y, s.size.x * sizeRate, s.size.y * sizeRate);
-        // circle(s.pos.x, s.pos.y, 12);
         fill(255);
-        // text(0.5 * (dist / size.l * 0.2), s.pos.x, s.pos.y);
     });
 }
